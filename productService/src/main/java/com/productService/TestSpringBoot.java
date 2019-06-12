aa902fd5ff3b48ee3ed510df5be0ab003f1ae28d
poojabisht <poojabishtd701@gmail.com>
2019-06-13 02:45:18 +0530

    product service

---
+++ productService/src/main/java/com/productService/TestSpringBoot.java
@@ -0,0 +1,24 @@
+package com.productService;
+
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+
+
+
+@SpringBootApplication(exclude= HibernateJpaAutoConfiguration.class)
+
+public class TestSpringBoot {
+    public static void main(String[] args)
+    {
+        SpringApplication.run( TestSpringBoot.class, args);
+    }
+
+
+
+
+}
